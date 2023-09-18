256597ad301032b03d0c6313635e23cdc4e644e9
ck@acedev <chandankumar75707@gmail.com>
2023-09-18 12:54:15 +0530

    password-encryption and decryption lib

--- Src/index.ts
+++ Src/index.ts
@@ -1,12 +1,78 @@
+import { ENCRYPTION_PASSWORD, PASSWORD, VERIFY_PASSWORD } from "./model";
+const crypto = require("crypto");
 export class Pass_Encr_Decr {
   constructor() {
-    function ALPassWordEncrytpion() {}
+    function ALPassWordEncrytpion(payload: PASSWORD) {
+      const algorithm = "aes-256-cbc";
+      const security_key = crypto
+        .createHmac("sha256", payload.secret_init_vector)
+        .update(payload.secret_key)
+        .digest("base64url")
+        .substring(0, 32);
+      const secret_init_vector = crypto
+        .createHmac("sha256", payload.secret_init_vector)
+        .update(payload.secret_key)
+        .digest("base64url")
+        .substring(0, 16);
 
-    function ALPassWordDecrytpion() {}
+      const cipher = crypto.createCipheriv(
+        algorithm,
+        security_key,
+        secret_init_vector
+      );
+      return Buffer.from(
+        cipher.update(payload.password, "utf8", "hex") + cipher.final("hex")
+      ).toString("base64"); // Encrypts data and converts to hex and base64
+    }
 
-    function ALVerifyPassWord() {}
+    function ALPassWordDecrytpion(payload: ENCRYPTION_PASSWORD) {
+      const algorithm = "aes-256-cbc";
+      const security_key = crypto
+        .createHmac("sha256", payload.secret_init_vector)
+        .update(payload.secret_key)
+        .digest("base64url")
+        .substring(0, 32);
+      const secret_init_vector = crypto
+        .createHmac("sha256", payload.secret_init_vector)
+        .update(payload.secret_key)
+        .digest("base64url")
+        .substring(0, 16);
+      const buff = Buffer.from(payload.encrypted_password, "base64");
+      const decipher = crypto.createDecipheriv(
+        algorithm,
+        security_key,
+        secret_init_vector
+      );
+      return (
+        decipher.update(buff.toString("utf8"), "hex", "utf8") +
+        decipher.final("utf8")
+      );
+    }
+
+    function ALVerifyPassWord(payload: VERIFY_PASSWORD) {
+      const algorithm = "aes-256-cbc";
+      const security_key = crypto
+        .createHmac("sha256", payload.secret_init_vector)
+        .update(payload.secret_key)
+        .digest("base64url")
+        .substring(0, 32);
+      const secret_init_vector = crypto
+        .createHmac("sha256", payload.secret_init_vector)
+        .update(payload.secret_key)
+        .digest("base64url")
+        .substring(0, 16);
+
+      const cipher = crypto.createCipheriv(
+        algorithm,
+        security_key,
+        secret_init_vector
+      );
+      const encrypted_password = Buffer.from(
+        cipher.update(payload.password, "utf8", "hex") + cipher.final("hex")
+      ).toString("base64"); // Encrypts data and converts to hex and base64
+      if (payload.encrypted_password === encrypted_password) {
+        return true;
+      }
+    }
   }
 }
-
-const PassEncrDecr = new Pass_Encr_Decr();
-module.exports = PassEncrDecr;
--- Src/model.ts
+++ Src/model.ts
@@ -1,12 +1,24 @@
-interface PASSWORD {
-    password:string,
-    secretKey:string,
-    algoritm:"aes-192-cbc"
+export interface PASSWORD {
+  password: string;
+  secret_key: string;
+  secret_init_vector: string | number;
 }
 
-interface ENCRYPTION_PASSWORD{
-encrypted_password:string |number,
-secret_key:string |number,
-algorithm:"aes-192-cbc"
+export interface ENCRYPTION_PASSWORD {
+  encrypted_password: string ;
+  secret_key: string | number;
+  secret_init_vector: string | number;
+}
 
+export interface DESCRYPT_PASSWORD {
+  encrypted_password: string | any;
+  secret_key: string;
+  secret_init_vector: string | number;
+}
+
+export interface VERIFY_PASSWORD {
+  encrypted_password: string;
+  secret_key: string;
+  password: string | any;
+  secret_init_vector: string | number;
 }
