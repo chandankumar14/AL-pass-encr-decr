03a1357efa7a8829a5b18d98495c26fe470ee8b7
ck@acedev <chandankumar75707@gmail.com>
2023-09-18 16:03:24 +0530

    explain all metod| function is Readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# AL-Password-Encryption -Decryption- library
+## AL-Password-Encryption -Decryption- library
+
+al-pass-encr-decr is tool that help  us to ecrypt and decrypt the password for security purpose 
+
+# Installation
+```bash
+npm install -g al-pass-encr-decr # or using npm: npm i al-pass-encr-decr
+```
+
+al-pass-encr-decr will be installed globally to your system path.
+
+## Features
+
+  * Encrypt Password
+  * Decrypt Password
+  * Verify Password
+
+```js
+const ALPassEncrDecr =  require("al-pass-encr-decr")
+// *********PassWord encryption Function *************
+function ALPassWordEncrytpion(){
+    const EncryptedPassWord = ALPassEncrDecr.ALPassWordEncrytpion(
+         password:string |any,
+         secret_key:string,    
+         secret_init_vector_key: string,
+         init_vector: string,
+         init_vector_key: string
+    ) 
+    console.log(EncryptedPassWord)
+    // out_put : N2Y1YWM1N2ZkYWMzNjcxNjA0OTVkZWZiODJmZjZlMDM=
+}
+
+
+function ALPassWordDecrytpion(){
+    const DecryptedPassWord = ALPassEncrDecr.ALPassWordDecrytpion(
+        encrypted_password: string,
+        secret_key: string,
+        secret_init_vector_key: string,
+        init_vector: string,
+        init_vector_key: string
+    ) 
+    console.log(DecryptedPassWord)
+    // out_put : Decrypted_password.....
+}
+
+function ALVerifyPassWord(){
+    const VerifyPassWord = ALPassEncrDecr.ALVerifyPassWord(
+        encrypted_pass: string,
+        secret_key: string,
+        secret_init_vector_key: string,
+        init_vector: string,
+        password: string |any,
+        init_vector_key: string
+    )
+     console.log(VerifyPassWord)
+    // out_put : True or False .....
+}
+
+```
--- Src/index.ts
+++ Src/index.ts
@@ -5,27 +5,31 @@ export function ALPassWordEncrytpion<Types>(
   secret_init_vector_key: Types,
   init_vector: Types,
   init_vector_key: Types
-): string {
-  const algorithm = "aes-256-cbc";
-  const security_key = crypto
-    .createHmac("sha256", secret_init_vector_key)
-    .update(secret_key)
-    .digest("base64url")
-    .substring(0, 32);
-  const secret_init_vector = crypto
-    .createHmac("sha256", init_vector)
-    .update(init_vector_key)
-    .digest("base64url")
-    .substring(0, 16);
+) {
+  try {
+    const algorithm = "aes-256-cbc";
+    const security_key = crypto
+      .createHmac("sha256", secret_init_vector_key)
+      .update(secret_key)
+      .digest("base64url")
+      .substring(0, 32);
+    const secret_init_vector = crypto
+      .createHmac("sha256", init_vector)
+      .update(init_vector_key)
+      .digest("base64url")
+      .substring(0, 16);
 
-  const cipher = crypto.createCipheriv(
-    algorithm,
-    security_key,
-    secret_init_vector
-  );
-  return Buffer.from(
-    cipher.update(password, "utf8", "hex") + cipher.final("hex")
-  ).toString("base64"); // Encrypts data and converts to hex and base64
+    const cipher = crypto.createCipheriv(
+      algorithm,
+      security_key,
+      secret_init_vector
+    );
+    return Buffer.from(
+      cipher.update(password, "utf8", "hex") + cipher.final("hex")
+    ).toString("base64"); // Encrypts data and converts to hex and base64
+  } catch (error) {
+    return error;
+  }
 }
 
 export function ALPassWordDecrytpion<Types>(
@@ -34,28 +38,32 @@ export function ALPassWordDecrytpion<Types>(
   secret_init_vector_key: Types,
   init_vector: Types,
   init_vector_key: Types
-): string {
-  const algorithm = "aes-256-cbc";
-  const security_key = crypto
-    .createHmac("sha256", secret_init_vector_key)
-    .update(secret_key)
-    .digest("base64url")
-    .substring(0, 32);
-  const secret_init_vector = crypto
-    .createHmac("sha256", init_vector)
-    .update(init_vector_key)
-    .digest("base64url")
-    .substring(0, 16);
-  const buff = Buffer.from(encrypted_password, "base64");
-  const decipher = crypto.createDecipheriv(
-    algorithm,
-    security_key,
-    secret_init_vector
-  );
-  return (
-    decipher.update(buff.toString("utf8"), "hex", "utf8") +
-    decipher.final("utf8")
-  );
+) {
+  try {
+    const algorithm = "aes-256-cbc";
+    const security_key = crypto
+      .createHmac("sha256", secret_init_vector_key)
+      .update(secret_key)
+      .digest("base64url")
+      .substring(0, 32);
+    const secret_init_vector = crypto
+      .createHmac("sha256", init_vector)
+      .update(init_vector_key)
+      .digest("base64url")
+      .substring(0, 16);
+    const buff = Buffer.from(encrypted_password, "base64");
+    const decipher = crypto.createDecipheriv(
+      algorithm,
+      security_key,
+      secret_init_vector
+    );
+    return (
+      decipher.update(buff.toString("utf8"), "hex", "utf8") +
+      decipher.final("utf8")
+    );
+  } catch (error) {
+    return error;
+  }
 }
 
 export function ALVerifyPassWord<Types>(
@@ -66,27 +74,31 @@ export function ALVerifyPassWord<Types>(
   password: Types,
   init_vector_key: Types
 ) {
-  const algorithm = "aes-256-cbc";
-  const security_key = crypto
-    .createHmac("sha256", secret_init_vector_key)
-    .update(secret_key)
-    .digest("base64url")
-    .substring(0, 32);
-  const secret_init_vector = crypto
-    .createHmac("sha256", init_vector)
-    .update(init_vector_key)
-    .digest("base64url")
-    .substring(0, 16);
+  try {
+    const algorithm = "aes-256-cbc";
+    const security_key = crypto
+      .createHmac("sha256", secret_init_vector_key)
+      .update(secret_key)
+      .digest("base64url")
+      .substring(0, 32);
+    const secret_init_vector = crypto
+      .createHmac("sha256", init_vector)
+      .update(init_vector_key)
+      .digest("base64url")
+      .substring(0, 16);
 
-  const cipher = crypto.createCipheriv(
-    algorithm,
-    security_key,
-    secret_init_vector
-  );
-  const encrypted_password = Buffer.from(
-    cipher.update(password, "utf8", "hex") + cipher.final("hex")
-  ).toString("base64"); // Encrypts data and converts to hex and base64
-  if (encrypted_pass === encrypted_password) {
-    return true;
+    const cipher = crypto.createCipheriv(
+      algorithm,
+      security_key,
+      secret_init_vector
+    );
+    const encrypted_password = Buffer.from(
+      cipher.update(password, "utf8", "hex") + cipher.final("hex")
+    ).toString("base64"); // Encrypts data and converts to hex and base64
+    if (encrypted_pass === encrypted_password) {
+      return true;
+    }
+  } catch (error) {
+    return error;
   }
 }
