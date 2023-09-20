0c6dd4c791b8449ab146a29f878c4ff7f857e0cc
ck@acedev <chandankumar75707@gmail.com>
2023-09-20 10:29:21 +0530

    new changes

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
-export declare function ALPassWordEncrytpion<Types>(password: Types, secret_key: Types, secret_init_vector_key: Types, init_vector: Types, init_vector_key: Types): string;
-export declare function ALPassWordDecrytpion<Types>(encrypted_password: Types | any, secret_key: Types, secret_init_vector_key: Types, init_vector: Types, init_vector_key: Types): string;
-export declare function ALVerifyPassWord<Types>(encrypted_pass: Types, secret_key: Types, secret_init_vector_key: Types, init_vector: Types, password: Types, init_vector_key: Types): true | undefined;
+export declare function ALPassWordEncrytpion<Types>(password: Types, secret_key: Types, secret_init_vector_key: Types, init_vector: Types, init_vector_key: Types): unknown;
+export declare function ALPassWordDecrytpion<Types>(encrypted_password: Types | any, secret_key: Types, secret_init_vector_key: Types, init_vector: Types, init_vector_key: Types): any;
+export declare function ALVerifyPassWord<Types>(encrypted_pass: Types, secret_key: Types, secret_init_vector_key: Types, init_vector: Types, password: Types, init_vector_key: Types): unknown;
